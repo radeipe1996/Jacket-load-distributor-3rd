5108cb6a8294ce63fca80c4886f0deb8f13c0f58
radeipe1996 <rauldeiraperez@gmail.com>
2026-01-09 10:49:22 +0100

    Update app3.py

--- app3.py
+++ app3.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import streamlit.components.v1 as components
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timezone
 import os
 
 # ----------------------------
