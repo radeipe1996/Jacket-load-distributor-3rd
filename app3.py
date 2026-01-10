91f8efb199be20ed92eb1614696e59ef86c61bc3
radeipe1996 <rauldeiraperez@gmail.com>
2026-01-10 21:15:36 +0100

    Update app3.py

--- app3.py
+++ app3.py
@@ -143,6 +143,7 @@ def leg_box(label, value, minimum):
         <strong>{label}</strong><br>
         {value:.1f}%<br>
         <span style="font-size:12px;">Min: {minimum:.1f}%</span>
+        <span style="font-size:12px;">P: {value:.1f} bar</span>
     </div>
     """
 
